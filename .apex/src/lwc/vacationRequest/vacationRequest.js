9e6fcf47763eaef08dcc1795e0181d76129a823f
Egor <egor2001845@gmail.com>
2022-07-22 14:38:29 +0300

    Initial commit. Created button to make requests.

---
+++ .apex/src/lwc/vacationRequest/vacationRequest.js
@@ -0,0 +1,19 @@
+import {LightningElement} from 'lwc';
+import  REQUEST_TYPE_FIELD from '@salesforce/schema/Vacation_Request__c.RequestType__c';
+import START_DATE_FIELD from '@salesforce/schema/Vacation_Request__c.StartDate__c';
+import END_DATE_FIELD from '@salesforce/schema/Vacation_Request__c.EndDate__c';
+
+
+export default class VacationRequest extends LightningElement {
+    modelWindow = false;
+    fields = [REQUEST_TYPE_FIELD, START_DATE_FIELD, END_DATE_FIELD];
+
+    openRequestWindow() {
+
+        this.modelWindow = true;
+    }
+
+    closeRequestWindow() {
+        this.modelWindow = false;
+    }
+}
