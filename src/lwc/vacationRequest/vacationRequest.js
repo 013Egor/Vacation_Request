d39ac75d6cb5ca7c3f3e74cd67d44a86d9adedb3
Egor <egor2001845@gmail.com>
2022-07-23 22:59:19 +0300

    a

--- src/lwc/vacationRequest/vacationRequest.js
+++ src/lwc/vacationRequest/vacationRequest.js
@@ -6,7 +6,7 @@ import  REQUEST_TYPE_FIELD from '@salesforce/schema/Vacation_Request__c.RequestT
 import START_DATE_FIELD from '@salesforce/schema/Vacation_Request__c.StartDate__c';
 import END_DATE_FIELD from '@salesforce/schema/Vacation_Request__c.EndDate__c';
 
-import NAME_FIELD from '@salesforce/schema/User.Alias';
+import NAME_FIELD from '@salesforce/schema/User.Name';
 
 import hasManager from '@salesforce/apex/ManagerController.hasManager';
 
