c7c6aafa703e29c6aff35546d20d962e74b6f08b
Egor <egor2001845@gmail.com>
2022-07-23 23:46:16 +0300

    clear html and js

--- src/lwc/vacationRequest/vacationRequest.js
+++ src/lwc/vacationRequest/vacationRequest.js
@@ -1,12 +1,9 @@
 import {LightningElement, wire} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { getSObjectValue } from '@salesforce/apex';
 
 import  REQUEST_TYPE_FIELD from '@salesforce/schema/Vacation_Request__c.RequestType__c';
 import START_DATE_FIELD from '@salesforce/schema/Vacation_Request__c.StartDate__c';
 import END_DATE_FIELD from '@salesforce/schema/Vacation_Request__c.EndDate__c';
-
-import NAME_FIELD from '@salesforce/schema/User.Name';
 
 import hasManager from '@salesforce/apex/ManagerController.hasManager';
 
@@ -15,36 +12,22 @@ export default class VacationRequest extends LightningElement {
 
     fields = [REQUEST_TYPE_FIELD, START_DATE_FIELD, END_DATE_FIELD];
 
-    _title = 'Sample Title';
-    message = 'Sample Message';
-    variant = 'error';
-    variants = 'success';
-
     @wire(hasManager) contact;
 
-    get text() {
-        return this.contact.data ? 'ok' : 'nope';
-    }
-
     openRequestWindow() {
-        this.modelWindow = true;
-    }
-
-    closeRequestWindow() {
-        this.modelWindow = false;
-    }
-
-    completeRecord(event) {
-
-        let isGood = setManager(event);
-        if (isGood == true) {
+        if (hasManager()) {
+            this.modelWindow = true;
+        } else {
             const evt = new ShowToastEvent({
-                title: this._title,
-                message: this.message,
-                variant: this.variants,
+                title: 'Error',
+                message: 'Добавьте менеджера',
+                variant: 'error'
             });
             this.dispatchEvent(evt);
         }
+    }
+
+    closeRequestWindow() {
         this.modelWindow = false;
     }
 }
