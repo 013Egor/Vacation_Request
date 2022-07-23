5625d3254ed38fbd063e11a1a0b3af19a366eb1b
Egor <egor2001845@gmail.com>
2022-07-23 18:54:33 +0300

    added trigger to VacationRequest__c to set manager

--- src/lwc/vacationRequest/vacationRequest.js
+++ src/lwc/vacationRequest/vacationRequest.js
@@ -5,9 +5,7 @@ import  REQUEST_TYPE_FIELD from '@salesforce/schema/Vacation_Request__c.RequestT
 import START_DATE_FIELD from '@salesforce/schema/Vacation_Request__c.StartDate__c';
 import END_DATE_FIELD from '@salesforce/schema/Vacation_Request__c.EndDate__c';
 
-import setManager from 'classes/ManagerController.setManager';
-
-
+import setManager from '@salesforce/apex/ManagerController.setManager';
 
 export default class VacationRequest extends LightningElement {
     modelWindow = false;
