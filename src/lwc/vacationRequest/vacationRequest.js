df0b90bb6c1383295c59d1ac159924b13d5fe43b
Egor <egor2001845@gmail.com>
2022-07-24 19:32:11 +0300

    added list of requests

--- src/lwc/vacationRequest/vacationRequest.js
+++ src/lwc/vacationRequest/vacationRequest.js
@@ -26,9 +26,10 @@ export default class VacationRequest extends LightningElement {
     handleLoad() {
 
         getRequests().then(result => {
+            result.forEach(x => x.put({UserName: 'Egor'}))
             this.requests = result;
-            this.requests.forEach(x => x.put({UserName: 'Egor'}))
-            console.log(this.requests);
+            //this.requests.forEach(x => x.put({UserName: 'Egor'}))
+            console.log(result);
         }).catch(error => {
             this.error = error;
         });
